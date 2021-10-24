61d23339276248b610278b815d5ebd19eb1bc198
Jfroel <92544899+Jfroel@users.noreply.github.com>
2021-10-23 18:21:08 -0700

    empty classes

---
+++ Hour.java
@@ -0,0 +1,25 @@
+public class Hour {
+    private int id;
+    private String task;
+    private boolean existTask;
+
+    public int getId() {
+        return id;
+    }
+    public String getTask() {
+        return task;
+    }
+    public boolean getExistTask() {
+        return existTask;        
+    }
+    public void setId(int x) {
+        id = x;
+    }
+    public void setTask(String s) {
+        task = s;
+    }
+    public void setExistTask(boolean b) {
+        existTask = b;        
+    }
+
+}
